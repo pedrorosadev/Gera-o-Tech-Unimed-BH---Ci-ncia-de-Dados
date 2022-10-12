51848f95e9c73c22fa39a78261778f2a2f559b07
pedrorosadev <pedrorosadev20@gmail.com>
2022-10-11 23:46:00 -0300

    Tuplas

---
+++ Estrutura de Dados/Tuplas/ex01.py
@@ -0,0 +1,12 @@
+frutas = ('Laranja', 'Pera', 'Uva')
+print(frutas)
+
+letras = tuple('python')
+print(letras)
+
+numeros = tuple([1,2,3,4])
+print(numeros)
+print(len(numeros))
+
+pais = ('Brasil')
+print(pais)
---
+++ Estrutura de Dados/Tuplas/ex03.py
@@ -0,0 +1,4 @@
+carros = ('gol', 'palio', 'celta')
+
+for index, carro in enumerate (carros):
+    print(f'{index + 1} : {carro}')
