04ea366008cb29364f728363b3ac9ac275d12691
pedrorosadev <pedrorosadev20@gmail.com>
2022-10-13 22:04:29 -0300

    Dicionarios

---
+++ Estrutura de Dados/Dicionarios/ex02.py
@@ -0,0 +1,11 @@
+dados = {'nome':'Guilherme', 'idade':28, 'telefone':'3333-1234'}
+
+dados['nome'] #'Guilherme'
+dados['idade'] # 28
+dados['telefone'] # '3333-1234'
+
+dados['nome'] = 'Maria'
+dados['idade'] = 18
+dados['telefone'] = '9988-1781'
+
+print(dados)
